fa9220730bf8aa65165009d9332bdfb240d8ab09
piercecave <piercecave@yahoo.com>
2020-02-17 23:55:06 -0800

    confusion

--- client/log_in/client/upgrade-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker rm -f web_client
-echo "✅  Current Docker Container Stopped & Removed"
-docker pull piercecave/web_client
-echo "✅  Lastest Docker Image Pulled To Server"
-docker run -d --name web_client -p 443:443 -p 80:80 -v /etc/letsencrypt:/etc/letsencrypt:ro piercecave/web_client
-echo "✅  Updated Docker Container Successfully Running"
